92de8571ec4f0a8f7fd2fe4afe7c6bbb8ab4f8ef
Johann1ee <johannlee03@gmail.com>
2024-08-15 16:35:38 -0700

    Added basic command functions

--- ShellScript.c
+++ ShellScript.c
@@ -94,6 +94,75 @@ void shell_loop(void){
     } while (status);
 }
 
+// SHELL COMMANDS 
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit",
+};
+
+int (*builtin_func[]) (char **) = {
+    &shell_cd,
+    &shell_help,
+    &shell_exit,
+};
+
+int shell_num_builtin() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+// SHELL COMMAND IMPLEMENTATION
+int shell_cd(char **args){
+    if (args[1] == NULL) {
+        fprintf(stderr, "SHELL: EXPECTED ARGUEMENTT TO \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0){
+            perror('lsh');
+        }
+    }
+    return 1;
+}
+
+int shell_help(char **args){
+    int i;
+    printf("Johann Lee's LSH\n");
+    printf("Type program anames and arguements and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < shell_num_builtin(); i++){
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int shell_exit(char **args){
+    return 0;
+}
+
+int shell_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < shell_num_builtin(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return shell_launch(args);
+}
+
 int main (int argc, char **argv){
 
     shell_loop(); // command loop for shell
